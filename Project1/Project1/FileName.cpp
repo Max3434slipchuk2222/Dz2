7a4ab40691bc41d66a0d4d1e6eec3a2841522a01
Slipchuk Max <maxslipchuk11@gmail.com>
2024-06-28 16:24:13 +0300

    Add Zmini

---
+++ Project1/Project1/FileName.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Windows.h>
+
+#include <ctime>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#define Size 50
+
+int main() {
+    srand(time(NULL));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char arr[Size]; 
+    char str[Size]; 
+    int b = 0;
+    int c = Size; 
+    int a = Size; 
+}
